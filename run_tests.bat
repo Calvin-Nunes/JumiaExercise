45cdfe04ac8a7d3c50dad8af545fb6068e0ad863
Calvin Nunes <35965305+Calvin-Nunes@users.noreply.github.com>
2022-01-05 20:52:54 -0300

    version 1.0.0

--- run_tests.bat
+++ run_tests.bat
@@ -1,2 +1,5 @@
 cd %~dp0
-npm run test
+@echo off
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+echo "Iniciando Testes"
+npm run test:unit
--- tests/unit/allTests.spec.js
+++ tests/unit/allTests.spec.js
@@ -1,7 +1,8 @@
-import { mount } from '@vue/test-utils'
-import Button from './components/Button'
-import ApiHelper from "static/libraries/ApiHelper";
-const LibUtils = require("static/libraries/libUtils").default
+import { mount } from '@vue/test-utils';
+const LibUtils = require("../../static/libraries/libUtils").default
+import Button from '../../components/Button';
+import ApiHelper from "../../static/libraries/ApiHelper";
+import axios from "axios";
 
 describe('Testing API', () => {
     const apiKey = 'hOjNh9CbqGgfNV8N7qawjcMchELLkL9X'
@@ -22,32 +23,38 @@ describe('Testing API', () => {
     it("Throws error if invalid endpoint", async () => {
         let endpoint = "/invalid"
         let apiUrl = apiHelper.buildRequestUrl(endpoint);
-        const response = await axios.get(apiUrl)
-        console.log(response)
-        expect(response.status).toBe(200);
+        const response = await axios.get(apiUrl).catch(er => null)
+        expect(response).toBe(null);
     })
 })
 
-describe('Testing Component Button', async () => {
+describe('Testing Component: Button', () => {
     const wrapper = mount(Button)
-    console.log(wrapper)
 
-    await wrapper.setProps({ caption: "Test Button" });
+    wrapper.setProps({ caption: "Test Button" });
 
     it('Button has caption "Test Button"', () => {
         expect(wrapper.vm.caption).toBe("Test Button")
         expect(wrapper.html()).toContain("Test Button")
     })
 
-    await wrapper.setProps({ disabled: true });
+    wrapper.setProps({ disabled: true });
 
     it('Disabled Button is really disabled', () => {
-        let buttonElement = wrapper.find("button")
-        expect(buttonElement.classList).toContain("disabled")
+        let buttonElement = wrapper.element
+        let classes = buttonElement.classList
+
+        expect(wrapper.vm.isDisabled).toBe(true);
+        expect(classes != null).toBe(true)
+        if (classes != null) {
+            expect(classes).toContain("disabled")
+        } else {
+            return false
+        }
     })
 })
 
-describe('Testing LibUtils functions', () => {
+describe('Testing LibUtils.js functions', () => {
 
     it('toDecimal returns 0 if invalid value is passed', () => {
         expect(LibUtils.toDecimal("a")).toBe(0)
